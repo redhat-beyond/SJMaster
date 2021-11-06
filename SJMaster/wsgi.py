7e299f3563522cde758fed5954ae80b63fb32d13
Yael Edreich <yaeledreich@gmail.com>
2021-11-06 09:22:23 +0200

    Fixed project's name created by django
    Fixed the django project file's name from "beyond_tutorial" to "SJMaster"
    Also, fixed "beyond_tutorial" to "SJMaster" inside - settings.py, asgi.py, urls.py and wsgi.py

--- SJMaster/wsgi.py
+++ SJMaster/wsgi.py
@@ -1,5 +1,4 @@
-"""
-WSGI config for beyond_tutorial project.
+"""WSGI config for SJMaster project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +10,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beyond_tutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SJMaster.settings')
 
 application = get_wsgi_application()
