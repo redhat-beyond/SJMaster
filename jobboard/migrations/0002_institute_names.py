61bc6d919c662479a430c8b650935b5d25dfdd04
guyy13 <91069555+guyy13@users.noreply.github.com>
2021-11-14 16:37:43 +0200

    Add migrations files and automation
    Add a migrations file to initalize the created models.
    Add a migrations file to populate the EducationalInstitutes
    table.
    Update setup.sh to run migrations on the VM's boot.

---
+++ jobboard/migrations/0002_institute_names.py
@@ -0,0 +1,35 @@
+from django.db import migrations, transaction
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('jobboard', '0001_initial'),
+    ]
+
+    def generate_data(apps, schema_editor):
+        from jobboard.models import EducationalInstitutes
+
+        institute_names = [
+            "Technion - institute of Technology", "Hebrew University of Jerusalem", "Weizmann Institute of science ",
+            "Bar Ilan University", "Tel Aviv University", "University of Haifa", "Ben Gurion University",
+            "Open university", "Ariel University", "Reichman University", "College of law and business - Ramat Gan",
+            "Academic college of Tel Aviv Yafo", "Afeka College of engineering, Tel Aviv", "Ashkelon Academic College",
+            "Bezalel Academy of Art and Deisgn", "Center for Academic studies, Or Yehuda",
+            "College of Management Academic Studies, Rishon Leziyon", "Dan Academic Studies",
+            "Hadassah Academic College, Jerusalem", "Holon Institute of Technology",
+            "Jerusalem Academy of Music and Dance", "Jerusalem College of Engineering",
+            "Jerusalem College of Techonlogy", "Kinneret Academic College", "Lander Institute, Jerusalem",
+            "Max Stern Academic College of Emek Yezreel", "Mivhar College, Bnei Brak", "Netanya Academic College",
+            "Netanya Academic College of Law", "Ono Academic of College, Kiryat Ono",
+            "ORT Braude college of Engineering, Karmiel", "Peres Academic Center, Rehovot", "Ruppin Academic Center",
+            "Sapir Academic College", "Sami Shmaoon College of Engineering", "Sha'arei Mishpat, Hod HaSharon",
+            "Shalem College, Jerusalem", "Shenkar College of Engineering and Design", "Tel Hai Academic College",
+            "Westren Galilee College, Acre", "Yehuda Regional College, Kiryat Araba", "Zefat Academic College"
+        ]
+        with transaction.atomic():
+            for name in institute_names:
+                EducationalInstitutes(institute_name=name).save()
+
+    operations = [
+        migrations.RunPython(generate_data),
+    ]
