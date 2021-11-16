2b582d240852e59791726912a533ecfac7ba24dc
benmenachem <benmenahem1212@gmail.com>
2021-11-16 20:50:01 +0200

    Create Recruiter models and migrations
    Created the following Recruiter models: Recruiter, Company
    Also created migration files to initialize the models

--- recruiter/models.py
+++ recruiter/models.py
@@ -1,3 +1,23 @@
-# from django.db import models
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=255)
+    company_description = models.TextField(max_length=500)
+    company_website_url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.company_name
+
+
+class Recruiter(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    company = models.ForeignKey(Company, on_delete=models.RESTRICT, related_name='recruiters')
+    email = models.EmailField(max_length=255)
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
