0e64ca11d2762d0925da0fdd5c52ae728d416753
guyy13 <91069555+guyy13@users.noreply.github.com>
2021-11-16 14:49:43 +0200

    Create Student models and migrations
    Created the following Student models: Student, EducationalInstitution .
    Also created migration files to initialize the models, and to populate the
    EducationalInstitution table.

--- student/models.py
+++ student/models.py
@@ -1,3 +1,23 @@
-# from django.db import models
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class EducationalInstitution(models.Model):
+    institution_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.institution_name
+
+
+class Student(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    date_of_birth = models.DateField()
+    phone_number = models.CharField(max_length=15, blank=True)
+    educational_institution_name = models.ManyToManyField(EducationalInstitution, related_name='students')
+    graduation_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
