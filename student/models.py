d4ae62a7de102be38cb16aa9b36e9ac8628014c6
guyy13 <91069555+guyy13@users.noreply.github.com>
2021-11-22 20:59:21 +0200

    Create student model functions and tests
    Create the following student models functions (and tests for them):
    get_institutions, get_institution_students, get_students,
    get_students_graduate_after

--- student/models.py
+++ student/models.py
@@ -8,6 +8,13 @@ class EducationalInstitution(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def get_institutions_with_enrolled_students(cls):
+        """
+        Return all institutions that have enrolled students as a QuerySet
+        """
+        return cls.objects.filter(students__isnull=False)
+
 
 class Student(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
@@ -21,3 +28,17 @@ class Student(models.Model):
 
     def __str__(self):
         return f'{self.full_name}'
+
+    @classmethod
+    def get_students_enrolled_at(cls, institutions):
+        """
+        Gets all the students that are enrolled at one of the given institutions
+        """
+        return cls.objects.filter(educational_institution__in=institutions)
+
+    @classmethod
+    def get_students_graduate_after(cls, year):
+        """
+        Gets all the students from the DB that graduate after a given year
+        """
+        return cls.objects.filter(graduation_date__year__gte=year)
