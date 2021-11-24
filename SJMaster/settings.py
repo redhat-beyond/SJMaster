9937dc7d39dc45959ee84a32c7913184cbc4e5fa
Yarin Bouzaglo <yarinbouzaglo1@gmail.com>
2021-11-24 15:56:36 +0200

    Create application app and add it to the project's installed apps.

--- SJMaster/settings.py
+++ SJMaster/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'student.apps.StudentConfig',
     'recruiter.apps.RecruiterConfig',
     'login.apps.LoginConfig',
+    'application.apps.ApplicationConfig',
 ]
 
 MIDDLEWARE = [
