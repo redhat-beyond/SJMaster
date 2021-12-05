4c44198e4a96cb5275c02824540eb7b1c424d21d
guyy13 <91069555+guyy13@users.noreply.github.com>
2021-12-05 18:04:59 +0200

    Create a navigation bar for the website

--- SJMaster/settings.py
+++ SJMaster/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'SJMaster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'SJMaster/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
