425658c1a72967adba1679a316c2138505a139fb
benmenachem <benmenahem1212@gmail.com>
2021-12-08 16:30:32 +0200

    Created a job creation form
    Updated the fields: company, recruiter and date to be filled in automatically and not shown to the recruiter
    filling the job application

--- recruiter/views.py
+++ recruiter/views.py
@@ -1,9 +1,17 @@
 # from django.shortcuts import render
 
 # Create your views here.
+
+from datetime import date
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic import UpdateView
+from django.shortcuts import render
+from django.views.generic import UpdateView, CreateView
+from jobboard.models import Job
 from recruiter.models import Recruiter
+
+
+def job_created_successfully(request):
+    return render(request, 'job_created_successfully.html')
 
 
 class UpdateRecruiterSettings(UserPassesTestMixin, UpdateView):
@@ -14,3 +22,21 @@ class UpdateRecruiterSettings(UserPassesTestMixin, UpdateView):
 
     def test_func(self):
         return self.request.user.id == int(self.kwargs['pk'])
+
+
+class CreateNewJobForm(CreateView, UserPassesTestMixin):
+    model = Job
+    fields = (
+        'title', 'job_type', 'work_from', 'description', 'city', 'address', 'title_keywords')
+    template_name = 'create_new_job_form.html'
+    success_url = '/job_created_successfully'
+
+    def test_func(self):
+        return Recruiter.is_recruiter(self.request.user.id)
+
+    def form_valid(self, form):
+        logged_recruiter = Recruiter.objects.get(user_id=self.request.user.id)
+        form.instance.recruiter = logged_recruiter
+        form.instance.company = logged_recruiter.company
+        form.instance.date_created = date.today()
+        return super(CreateNewJobForm, self).form_valid(form)
