257240b4d75c3cfd1fb5f2e5a76a7cf9dc187ac5
benmenachem <benmenahem1212@gmail.com>
2021-12-08 16:30:32 +0200

    Added a recruiter settings page
    The page allows recruiter to modify his information

--- recruiter/views.py
+++ recruiter/views.py
@@ -1,3 +1,16 @@
 # from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import UpdateView
+from recruiter.models import Recruiter
+
+
+class UpdateRecruiterSettings(UserPassesTestMixin, UpdateView):
+    model = Recruiter
+    fields = ('name', 'company', 'email', 'phone_number')
+    template_name = 'recruiter_account_settings.html'
+    success_url = '/'
+
+    def test_func(self):
+        return self.request.user.id == int(self.kwargs['pk'])
