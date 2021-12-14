514c648fa7c4b9353cac920a9186cdf5a859d9f8
guyy13 <91069555+guyy13@users.noreply.github.com>
2021-12-14 09:08:21 +0200

    Create a "My Jobs" page for recruiters
    Create a My Jobs page for recruiters that displays all the jobs the
    logged in recruiter submitted, and a short summary for each applicant.

--- SJMaster/urls.py
+++ SJMaster/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from jobboard.views import board
 from login.views import register_request
-from recruiter.views import UpdateRecruiterSettings, CreateNewJobForm, job_created_successfully
-
+from recruiter.views import UpdateRecruiterSettings, CreateNewJobForm, job_created_successfully, \
+    recruiter_view_my_jobs_and_applications
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +28,6 @@ urlpatterns = [
     path('recruiter/account_settings/<slug:pk>', UpdateRecruiterSettings.as_view(),
          name='recruiter_account_settings'),
     path('recruiter/create_new_job_form/', CreateNewJobForm.as_view(), name='create_new_job_form'),
-    path('job_created_successfully/', job_created_successfully, name='job_created')
+    path('job_created_successfully/', job_created_successfully, name='job_created'),
+    path('myjobs', recruiter_view_my_jobs_and_applications, name='myjobs')
 ]
